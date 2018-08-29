0fadb60ca73c6ad692c00e2f7f323f50ce1844de
el <36127111+mountis@users.noreply.github.com>
2018-08-28 21:45:54 -0400

    FIX FOR RESUBMIT

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/TestFetchJoke.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/TestFetchJoke.java
@@ -1,18 +1,28 @@
 package com.udacity.gradle.builditbigger;
 
 
+import android.util.Log;
+
+import java.util.concurrent.ExecutionException;
+
 import static junit.framework.TestCase.assertNotNull;
-import static junit.framework.TestCase.assertNotSame;
+import static junit.framework.TestCase.assertTrue;
 
 
 public class TestFetchJoke extends AndroidTestCase {
-    public void test(){
-        new FetchJoke(new Listener() {
-            @Override
-            public void onJokeLoaded(String joke) {
-                assertNotNull(joke);
-                assertNotSame(joke,"");
-            }
-        }).execute();
+    private static final String TAG = TestFetchJoke.class.getSimpleName();
+    private Listener mListner;
+
+    public void testFetchesNonEmptyString() {
+        FetchJoke fetchJoke = new FetchJoke( mListner );
+        fetchJoke.execute();
+        try {
+            String joke = fetchJoke.get();
+            Log.d( TAG, "Joke text: " + joke );
+            assertNotNull( joke );
+            assertTrue( joke.length() > 0 );
+        } catch (InterruptedException | ExecutionException e) {
+            Log.e( TAG, Log.getStackTraceString( e ) );
+        }
     }
 }
